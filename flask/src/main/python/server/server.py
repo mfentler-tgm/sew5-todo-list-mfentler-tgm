9073a900ca0691b188372161898d9163815bcf45
Mario Fentler <mfentler@student.tgm.ac.at>
2019-03-22 08:52:52 -0700

    Added sqlalchemy and marshmallow

--- flask/src/main/python/server/server.py
+++ flask/src/main/python/server/server.py
@@ -1,12 +1,59 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task.db'
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-tasks = {}
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+
+class TaskSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ("id", "name", "description")
+
+task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
 
 @app.route('/',methods=['GET'])
 def get():
-    return tasks
+    return 'Tasks: '
+
+@app.route('/',methods=['POST'])
+def post():
+    name = request.format['name']
+    description = request.format['description']
+
+    t = Task(name,description)
+    db.session.add(t)
+    db.session.commit()
+    return "Sucessfully added a new task"
 
 if __name__=="__main__":
-    app.run(debug=True)
+    db.create_all()
+    try:
+        t = Task('testTask','db auffuellen')
+        print("created task")
+        print(t)
+        db.session.add(t)
+        db.session.commit()
+    except:
+        pass
+    finally:
+        tasks = Task.query.all()
+        tasks_schema.dump(tasks)
+
+        app.run()
