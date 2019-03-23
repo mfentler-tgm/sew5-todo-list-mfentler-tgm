e38f1d3a6433245b418b07ab4c08610843bea690
Mario Fentler <mfentler@student.tgm.ac.at>
2019-03-23 10:37:17 -0700

    CRUD works now

--- flask/src/main/python/server/server.py
+++ flask/src/main/python/server/server.py
@@ -30,31 +30,63 @@ task_schema = TaskSchema()
 tasks_schema = TaskSchema(many=True)
 
 @app.route('/',methods=['GET'])
-def get():
-    all_users = Task.query.all()
-    result = tasks_schema.dump(all_users)
-    #print(jsonify(result.data))
+def get_all():
+    all_tasks = Task.query.all()
+    result = tasks_schema.dump(all_tasks)
+    return jsonify(result.data)
+
+@app.route('/<id>', methods=['GET'])
+def get(id):
+    task = Task.query.filter_by(id=id).first()
+    result = task_schema.dump(task)
     return jsonify(result.data)
 
 @app.route('/',methods=['POST'])
 def post():
     if "name" not in request.json:
-        print("nix da")
+        raise ValueError("Name is a must have")
+    if "description" not in request.json:
+        raise ValueError("Task description is a must have")
+
     name = request.json["name"]
     description = request.json["description"]
 
     t = Task(name,description)
     db.session.add(t)
     db.session.commit()
-    return "Sucessfully added a new task"
+    result = task_schema.dump(t)
+    return jsonify(result.data)
+
+@app.route('/<id>', methods=['PUT'])
+def put(id):
+    task = Task.query.filter_by(id=id).first()
+    name = task.name
+    description = task.description
+    print(request.json)
+    if "name" in request.json:
+        name = request.json["name"]
+    if "description" in request.json:
+        description = request.json["description"]
+
+    task.name = name
+    task.description = description
+    db.session.commit()
+    result = task_schema.dump(task)
+    return jsonify(result.data)
+
+@app.route('/<id>', methods=['DELETE'])
+def delete(id):
+    task = Task.query.filter_by(id=id).first()
+    db.session.delete(task)
+    db.session.commit()
+    result = task_schema.dump(task)
+    return jsonify(result.data)
 
 if __name__=="__main__":
     db.drop_all()
     db.create_all()
     try:
         t = Task('testTask','db auffuellen')
-        print("created task")
-        print(t)
         db.session.add(t)
         db.session.commit()
     except:
